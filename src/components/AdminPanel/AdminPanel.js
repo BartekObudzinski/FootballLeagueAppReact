16a1c65e345b2ca7d45fa5a37142f8c81a237610
Holinice <obudzinskib@gmail.com>
2021-04-19 09:22:49 +0200

    initial commit for panel-admin

--- src/components/AdminPanel/AdminPanel.js
+++ src/components/AdminPanel/AdminPanel.js
@@ -1,9 +1,30 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Accordion, Card } from "react-bootstrap";
+import { BsPlusSquare } from "react-icons/bs";
 const AdminPanel = () => {
   return (
     <Container>
-      <h1>Admin panel</h1>
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            <BsPlusSquare /> Add a match!
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>tu se normlanie bede mecza dodawal</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="1">
+            <BsPlusSquare /> Change a result!
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>
+              a tu se mi sie wyrenderuja mecze i se bede je wybieral po dacie i
+              zmienial im wynik es{" "}
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
     </Container>
   );
 };
